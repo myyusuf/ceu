c96eff7d211fe9a3199be59adaa94f337d0ec672
myyusuf <myyusuf1911@gmail.com>
2017-04-06 14:42:40 +0700

    init reactjs

---
+++ handlers/pages.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const Wreck = require('wreck');
+
+exports.home = function (request, reply) {
+
+  // if (request.auth.isAuthenticated) {
+  //   reply.view('index', {
+  //       user: request.auth.credentials.username,
+  //       role: request.auth.credentials.scope[0]
+  //   });
+  // }else{
+  //   return reply.redirect(this.webBaseUrl + '/login');
+  // }
+
+  reply.view('index', {});
+
+};
+
+// exports.login = function (request, reply) {
+//     reply.view('login', {}, {layout: 'login_layout'});
+// };
+//
+// exports.projects = function (request, reply) {
+//
+//     const apiUrl = this.apiBaseUrl + '/projects';
+//     const token = request.auth.credentials.token;
+//
+//     var params = '?pagenum=' + request.query.pagenum + "&pagesize=" + request.query.pagesize +
+//     "&searchTxt=" + request.query.searchTxt;
+//
+//     var url = apiUrl + params;
+//
+//     Wreck.get(url, {
+//       json: true,
+//       headers: {
+//           'Authorization': 'Bearer ' + token
+//       }
+//     }, (err, res, payload) => {
+//
+//         if (err) {
+//             throw err;
+//         }
+//         console.log('projects : ' + JSON.stringify(payload));
+//         reply(payload);
+//     });
+// };
--- routes.js
+++ routes.js
@@ -1,11 +1,17 @@
 'use strict';
 
+const Pages = require('./handlers/pages');
+const Assets = require('./handlers/assets');
+
 module.exports = [
   {
     method: 'GET',
     path: '/',
-    handler: function(request, reply){
-      reply({result: 'OK'});
-    }
+    handler: Pages.home
+  },
+  {
+      method: 'GET',
+      path: '/{param*}',
+      handler: Assets.servePublicDirectory
   }
 ];
